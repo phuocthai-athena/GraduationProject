d0dcfe70f578e8abe6bb7b8c36aadf9b0cbbf343
phuocthai-athena <89267473+phuocthai-athena@users.noreply.github.com>
2022-03-17 22:19:58 +0700

    thai

--- Backend/src/migrations/migration-create-user.js
+++ Backend/src/migrations/migration-create-user.js
@@ -37,7 +37,7 @@ module.exports = {
         type: Sequelize.STRING,
       },
       gender: {
-        type: Sequelize.BOOLEAN,
+        type: Sequelize.STRING,
       },
       image: {
         type: Sequelize.STRING,
