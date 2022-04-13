8d4887b2c566d60549b0c359bb52def567e24815
MinhTruong <truong29102000@gmail.com>
2022-04-13 21:40:17 +0700

    Finish #99 --

--- Frontend/src/containers/System/Doctor/ManagePatient.js
+++ Frontend/src/containers/System/Doctor/ManagePatient.js
@@ -4,86 +4,133 @@ import { FormattedDate, FormattedMessage } from "react-intl";
 import DatePicker from "../../../components/Input/DatePicker";
 import "./ManagePatient.scss";
 import { getAllPatientForDoctor } from "../../../services/userService";
+import moment from "moment";
 
 class ManagePatient extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            currentDate: new Date(),
-        };
-    }
-
-    async componentDidMount() {
-        let { user } = this.props;
-        let { currentDate } = this.state;
-        let formatedDate = new Date(currentDate).getTime();;
-        console.log("check state: ", this.state);
-        let res = await getAllPatientForDoctor({
-            doctorId: user.id,
-            date: formatedDate,
-        })
-        console.log("check res: ", res);
-    }
-
-    async componentDidUpdate(prevProps, prevState, snapshot) {
-        if (this.props.language !== prevProps.language) {
-        }
-    }
-
-    handleOnChangeDatePicker = (date) => {
-        this.setState({
-            currentDate: date[0],
-        });
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentDate: moment(new Date()).startOf("day").valueOf(),
+      dataPatient: []
     };
 
-    render() {
-        console.log(">>>>", this.props);
-        return (
-            <div className="manage-patient-container">
-                <div className="m-p-title">
-                    Quản lý bệnh nhân khám bệnh
-                </div>
+  }
 
-                <div className="manage-patient-body row">
-                    <div className="col-4 form-group">
-                        <label>Chọn ngày khám</label>
-                        <DatePicker
-                            onChange={this.handleOnChangeDatePicker}
-                            className="form-control"
-                            value={this.state.currentDate}
-                        />
-                    </div>
-                    <div className="col-12 table-manage-patient">
-                        <table style={{ width: '100%' }}>
-                            <tbody>
-                                <tr>
-                                    <th>Name</th>
-                                    <th colSpan="2">Telephone</th>
-                                </tr>
-                                <tr>
-                                    <td>Bill Gates</td>
-                                    <td>1444444</td>
-                                    <td>1888880</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
 
-            </div>
-        );
+
+  async componentDidMount() {
+    let { user } = this.props;
+    let { currentDate } = this.state;
+    let formatedDate = new Date(currentDate).getTime();
+    this.getDataPatient(user, formatedDate);
+  }
+
+  getDataPatient = async (user, formatedDate) => {
+    let res = await getAllPatientForDoctor({
+      doctorId: user.id,
+      date: formatedDate,
+    })
+    if (res && res.errCode === 0) {
+      this.setState({
+        dataPatient: res.data
+      })
     }
+  }
+
+  async componentDidUpdate(prevProps, prevState, snapshot) {
+    if (this.props.language !== prevProps.language) {
+    }
+  }
+
+  handleOnChangeDatePicker = (date) => {
+    this.setState({
+      currentDate: date[0],
+    }, () => {
+      let { user } = this.props;
+      let { currentDate } = this.state;
+      let formatedDate = new Date(currentDate).getTime();
+      this.getDataPatient(user, formatedDate);
+    });
+  };
+
+  handleBtnConfirm = () => { };
+
+  handleBtnRemedy = () => { };
+
+  render() {
+    console.log(">>>>", this.state);
+    let { dataPatient } = this.state;
+    return (
+      <div className="manage-patient-container">
+        <div className="m-p-title">
+          Quản lý bệnh nhân khám bệnh
+        </div>
+
+        <div className="manage-patient-body row">
+          <div className="col-4 form-group">
+            <label>Chọn ngày khám</label>
+            <DatePicker
+              onChange={this.handleOnChangeDatePicker}
+              className="form-control"
+              value={this.state.currentDate}
+            />
+          </div>
+          <div className="col-12 table-manage-patient">
+            <table style={{ width: '100%' }}>
+              <tbody>
+                <tr>
+                  <th>STT</th>
+                  <th>Thời gian</th>
+                  <th>Họ tên</th>
+                  <th>Địa chỉ</th>
+                  <th>Giới tính</th>
+                  <th>Actions</th>
+                </tr>
+                {dataPatient && dataPatient.length > 0 ?
+                  dataPatient.map((item, index) => {
+                    return (
+                      <tr key={index}>
+                        <td>{index + 1}</td>
+                        <td>{item.timeTypeDataPatient.valueVi}</td>
+                        <td>{item.patientData.firstName}</td>
+                        <td>{item.patientData.address}</td>
+                        <td>{item.patientData.genderData.valueVi}</td>
+                        <td>
+                          <button className="mb-btn-confirm"
+                            onClick={() => this.handleBtnConfirm()}
+                          >Xác nhận</button>
+                          <button className="mb-btn-remedy"
+                            onClick={() => this.handleBtnRemedy()}
+                          >Gửi hóa đơn</button>
+                        </td>
+
+                      </tr>
+                    )
+                  })
+                  : <tr>
+                    No data
+                  </tr>
+                }
+
+              </tbody>
+            </table>
+          </div>
+        </div>
+
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
-    return {
-        language: state.app.language,
-        user: state.user.userInfo,
-    };
+  return {
+    language: state.app.language,
+    user: state.user.userInfo,
+  };
 };
 
 const mapDispatchToProps = (dispatch) => {
-    return {};
+  return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ManagePatient);
