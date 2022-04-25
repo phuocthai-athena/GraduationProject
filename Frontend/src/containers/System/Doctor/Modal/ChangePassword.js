639ec2ecb7ee5ddc17eb6ebfb59c99ebb75cf232
phuocthai-athena <89267473+phuocthai-athena@users.noreply.github.com>
2022-04-25 17:39:14 +0700

    change password

--- Frontend/src/containers/System/Doctor/Modal/ChangePassword.js
+++ Frontend/src/containers/System/Doctor/Modal/ChangePassword.js
@@ -3,14 +3,28 @@ import { connect } from "react-redux";
 import { FormattedMessage } from "react-intl";
 import { Modal } from "reactstrap";
 import "./ChangePassword.scss";
+import { LANGUAGES } from "../../../../utils";
+import { toast } from "react-toastify";
+import { getPassword } from "../../../../services/userService";
+import bcrypt from "bcrypt";
 
 class ChangePassword extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      isShowOldPassword: false,
+      isShowNewPassword: false,
+      isShowNewConfirmPassword: false,
+      currentPassword: "",
+      oldPassword: "",
+      newPassword: "",
+      newConfirmPassword: "",
+    };
   }
 
-  async componentDidMount() {}
+  async componentDidMount() {
+    this.callApiGetPassword();
+  }
 
   async componentDidUpdate(prevProps, prevState, snapshot) {
     if (this.props.language !== prevProps.language) {
@@ -21,8 +35,65 @@ class ChangePassword extends Component {
     this.props.toggleFromParent();
   };
 
+  handleShowHideOldPassword = () => {
+    this.setState({
+      isShowOldPassword: !this.state.isShowOldPassword,
+    });
+  };
+
+  handleShowHideNewPassword = () => {
+    this.setState({
+      isShowNewPassword: !this.state.isShowNewPassword,
+    });
+  };
+
+  handleShowHideNewConfirmPassword = () => {
+    this.setState({
+      isShowNewConfirmPassword: !this.state.isShowNewConfirmPassword,
+    });
+  };
+
+  checkValidateInput = () => {
+    let isValid = true;
+    let { language } = this.props;
+    let arrCheck = ["oldPassword", "newPassword", "newConfirmPassword"];
+    for (let i = 0; i < arrCheck.length; i++) {
+      if (!this.state[arrCheck[i]]) {
+        isValid = false;
+        if (language === LANGUAGES.VI) {
+          toast.error("bạn chưa nhập trường " + arrCheck[i]);
+        } else {
+          toast.error("Not yet entered " + arrCheck[i]);
+        }
+        break;
+      }
+    }
+    return isValid;
+  };
+
+  isCheckOldPassword = (currentPassword, oldPassword) => {
+    if (currentPassword && oldPassword && currentPassword === oldPassword)
+      return true;
+    return false;
+  };
+
+  isCheckPasswordConfirmed = (newPassword, newConfirmPassword) => {
+    if (newPassword && newConfirmPassword && newPassword === newConfirmPassword)
+      return true;
+    return false;
+  };
+
+  callApiGetPassword = async () => {
+    let res = await getPassword(this.props.userInfo.id);
+    this.setState({
+      currentPassword: res.data.password,
+    });
+    console.log(this.state.currentPassword);
+  };
+
   render() {
     let { isOpenModal, closeModal } = this.props;
+    let { oldPassword, newPassword, newConfirmPassword } = this.state;
     return (
       <Modal
         isOpen={isOpenModal}
@@ -45,11 +116,82 @@ class ChangePassword extends Component {
               <i className="fas fa-times"></i>
             </span>
           </div>
-          <div className="change-password-body">inside modal body</div>
+          <div className="change-password-body">
+            <form>
+              <div class="mb-3">
+                <label className="form-label">Mật khẩu cũ</label>
+                <div className="custom-input-password">
+                  <input
+                    className="form-control"
+                    type={this.state.isShowOldPassword ? "text" : "password"}
+                    value={oldPassword}
+                    onChange={(event) =>
+                      this.onChangeInput(event, "oldPassword")
+                    }
+                  />
+                  <span onClick={() => this.handleShowHideOldPassword()}>
+                    <i
+                      className={
+                        this.state.isShowOldPassword
+                          ? "far fa-eye"
+                          : "far fa-eye-slash"
+                      }
+                    ></i>
+                  </span>
+                </div>
+              </div>
+              <div class="mb-3">
+                <label className="form-label">Mật khẩu mới</label>
+                <div className="custom-input-password">
+                  <input
+                    className="form-control"
+                    type={this.state.isShowNewPassword ? "text" : "password"}
+                    value={newPassword}
+                    onChange={(event) =>
+                      this.onChangeInput(event, "newPassword")
+                    }
+                  />
+                  <span onClick={() => this.handleShowHideNewPassword()}>
+                    <i
+                      className={
+                        this.state.isShowNewPassword
+                          ? "far fa-eye"
+                          : "far fa-eye-slash"
+                      }
+                    ></i>
+                  </span>
+                </div>
+              </div>
+              <div class="mb-3">
+                <label className="form-label">Xác nhận mật khẩu mới</label>
+                <div className="custom-input-password">
+                  <input
+                    className="form-control"
+                    type={
+                      this.state.isShowNewConfirmPassword ? "text" : "password"
+                    }
+                    value={newConfirmPassword}
+                    onChange={(event) =>
+                      this.onChangeInput(event, "newConfirmPassword")
+                    }
+                  />
+                  <span onClick={() => this.handleShowHideNewConfirmPassword()}>
+                    <i
+                      className={
+                        this.state.isShowNewConfirmPassword
+                          ? "far fa-eye"
+                          : "far fa-eye-slash"
+                      }
+                    ></i>
+                  </span>
+                </div>
+              </div>
+            </form>
+          </div>
           <div className="change-password-footer">
-            <button className="btn-change-password-confirm">Xác nhận</button>
+            <button className="btn btn-primary">Xác nhận</button>
             <button
-              className="btn-change-password-cancel"
+              className="btn btn-secondary"
               onClick={() => {
                 this.toggle();
               }}
@@ -66,6 +208,7 @@ class ChangePassword extends Component {
 const mapStateToProps = (state) => {
   return {
     language: state.app.language,
+    userInfo: state.user.userInfo,
   };
 };
 
